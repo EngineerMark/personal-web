c7a88ca13bfafb60a40c389396b36f4dd69b195a
Mark Meijer <engineermarkpro@gmail.com>
2022-06-15 12:33:28 +0200

    prepped dark theme

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import React from "react";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div>
+      <div class="md:container md:mx-auto mt-4">
+        <div class="max-w rounded overflow-hidden shadow-lg bg-gray-800">
+          
+        </div>
+      </div>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,7 @@ module.exports = {
   plugins: [
     require('tw-elements/dist/plugin')
   ],
+  darkMode: "class",
   theme: {
     extend: {},
   }
