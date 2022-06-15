b4901e163970d95f2754f310a2c1d123c6d3e117
Mark Meijer <engineermarkpro@gmail.com>
2022-06-15 22:30:58 +0200

    Added recent repo activity

--- src/App.js
+++ src/App.js
@@ -1,13 +1,44 @@
 import React from "react";
+import Moment from 'react-moment';
+import {useEffect, useState} from 'react';
 import { Helmet } from "react-helmet";
 import './App.css';
 
 function App() {
+  const [githubData, setGithubData] = useState(null);
+  const [githubUser, setGithubUser] = useState("EngineerMark");
+
+  async function fetchData(){
+    await fetch(`https://api.github.com/users/${githubUser}/repos`)
+      .then((response) => response.json())
+      .then(async (data) => {
+        data.sort((a,b)=>{
+          return ((a.pushed_at<a.updated_at)?
+            (a.pushed_at==b.pushed_at)?0:((a.pushed_at>b.pushed_at?-1:1)):
+            (a.updated_at==b.updated_at)?0:((a.updated_at>b.updated_at?-1:1)));
+        });
+
+        data = data.slice(0,5);
+
+        for(const repo of data){
+          await fetch(repo.events_url).then((_response)=> _response.json()).then((data)=>{
+            repo.events = data.slice(0,5);
+          });
+        }
+        setGithubData(data);
+        }
+      );
+  }
+
   const socials = {
     twitter: "https://twitter.com/id2amayakase",
-    github: "https://github.com/EngineerMark",
+    github: "https://github.com/"+githubUser,
     youtube: "https://www.youtube.com/c/Amayakase",
   };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   return (
     <div>
@@ -34,6 +65,48 @@ function App() {
                 <svg class="w-5 h-5 fill-current" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" /></svg>
               </a>
             </div>
+            <div class="text-left">
+              {githubData?<>
+                <div class="mt-8">
+                  <p class="text-xl text-white">Development Activity</p>
+                  <div>
+                    {
+                      githubData.map(repo=>(
+                        repo.events&&repo.events.length>0?
+                          <div class="flex justify-center mt-4 mb-4">
+                            <div class="block p-6 rounded-lg shadow-lg bg-white w-full">
+                              <h5 class="text-gray-900 text-xl leading-tight font-medium mb-2">{repo.full_name}</h5>
+                              <p class="text-gray-700 text-base mb-4">
+                                {
+                                  (
+                                    ()=>{
+                                      switch(repo.events[0].type){
+                                        case "CreateEvent":
+                                          return "Created repository";
+                                        case "ForkedEvent":
+                                          return "Forked repository";
+                                        case "PushEvent":
+                                          return <GithubPushEvent event={repo.events[0]} />
+                                        case "IssuesEvent":
+                                          return <GithubIssuesEvent event={repo.events[0]} />;
+                                        case "PublicEvent":
+                                          return <GithubPublicEvent event={repo.events[0]} />
+                                        default:
+                                          return "Updated repository";
+                                      }
+                                    }
+                                  )()
+                                }
+                              </p>
+                              <p><Moment fromNow>{repo.events[0].created_at}</Moment></p>
+                            </div>
+                          </div>:<></>
+                      ))
+                    }
+                  </div>
+                </div>
+              </>:null}
+            </div>
           </div>
         </div>
       </div>
@@ -42,3 +115,26 @@ function App() {
 }
 
 export default App;
+
+function GithubPublicEvent(props){
+  return (
+    <>Made repository public</>
+  )
+ }
+
+function GithubPushEvent(props){
+  return (
+    <>
+      <p>Pushed <a class="text-xs inline-block py-1 px-1 leading-none text-center whitespace-nowrap align-baseline font-bold bg-blue-600 hover:bg-blue-800 text-white rounded" href={"https://github.com/"+props.event.repo.name+"/commit/"+props.event.payload.commits[0].sha} target="_blank" rel="noreferrer">{props.event.payload.commits[0].sha.slice(0,7)}</a> on branch <span class="text-xs inline-block py-1 px-1 leading-none text-center whitespace-nowrap align-baseline font-bold bg-blue-600 hover:bg-blue-800 text-white rounded">{props.event.payload.ref}</span></p>
+      <p class="text-xs">Description: {props.event.payload.commits[0].message}</p>
+    </>
+  )
+}
+
+function GithubIssuesEvent(props){
+  return (
+    <>
+      {props.event.payload.action.charAt(0).toUpperCase() + props.event.payload.action.slice(1)} issue <a class="text-xs inline-block py-1 px-1 leading-none text-center whitespace-nowrap align-baseline font-bold bg-blue-600 hover:bg-blue-800 text-white rounded" href={props.event.payload.issue.html_url} target="_blank" rel="noreferrer">{props.event.payload.issue.title}</a>
+    </>
+  )
+}
